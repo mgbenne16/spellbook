1bf2e1fa1195b9381c87b8101c7748ab3d7fbc32
Megan Bennett <mgbenne16@earlham.edu>
2018-06-04 21:40:40 -0400

    Hmwk1

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button')
+
+const sayContents = function() {
+  const heading = document.querySelector('h1')
+  heading.textContent = 'Heading has been changed'
+}
+
+button.addEventListener('click', sayContents)
+Heading.innerText = "New Heading"
+
+sayContents()
+alert(x)
+
